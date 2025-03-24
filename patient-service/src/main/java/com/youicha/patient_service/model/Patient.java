ff59c16b0cd9be420fc4f8335f9935f3ac9fc5ed
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-03-24 22:54:20 +0100

    Patient class definition

--- patient-service/src/main/java/com/youicha/patient_service/model/Patient.java
+++ patient-service/src/main/java/com/youicha/patient_service/model/Patient.java
@@ -1,6 +1,33 @@
 package com.youicha.patient_service.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "patients")
 public class Patient {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
 }
