9179c60c1c8c4094206ee4e21dc98da3acf072f3
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-03-24 23:07:19 +0100

    Patient controller

---
+++ patient-service/src/main/java/com/youicha/patient_service/controller/PatientController.java
@@ -0,0 +1,41 @@
+package com.youicha.patient_service.controller;
+
+import com.youicha.patient_service.model.Patient;
+import com.youicha.patient_service.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Patient> getPatientById(@PathVariable Long id) {
+        return patientService.getPatientById(id);
+    }
+
+    @PostMapping
+    public Patient createPatient(@RequestBody Patient patient) {
+        return patientService.savePatient(patient);
+    }
+
+    @PutMapping("/{id}")
+    public Patient updatePatient(@PathVariable Long id, @RequestBody Patient patient) {
+        return patientService.updatePatient(id, patient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@RequestBody Patient patient){
+        patientService.deletePatient(patient);
+    }
+}
