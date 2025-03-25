0f5a4f4a75a88dbe14f9d6b007e0f4fc3e6e827c
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-03-25 23:53:05 +0100

    DTO for Patient and Note

---
+++ risk-service/src/main/java/com/youicha/risk_service/model/Note.java
@@ -0,0 +1,33 @@
+package com.youicha.risk_service.model;
+
+
+public class Note {
+
+    private String id;
+    private Long patientId;
+    private String noteText;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+}
---
+++ risk-service/src/main/java/com/youicha/risk_service/model/Patient.java
@@ -0,0 +1,55 @@
+package com.youicha.risk_service.model;
+
+import java.time.LocalDate;
+
+public class Patient {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String gender;
+
+    // Getters et setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
