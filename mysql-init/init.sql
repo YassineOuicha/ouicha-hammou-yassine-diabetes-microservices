4f8e3d76ec70fd9c5126622df4db0cc585334de0
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-03-31 21:28:55 +0200

    updating routin and docker-compose

---
+++ mysql-init/init.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS patientdb;
+USE patientdb;
+
+CREATE TABLE IF NOT EXISTS patients (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    address VARCHAR(255) DEFAULT NULL,
+    phone_number VARCHAR(20) DEFAULT NULL
+);
+
+INSERT INTO patients (first_name, last_name, birth_date, gender, address, phone_number)
+VALUES
+    ('TestNone', 'Test', '1966-12-31', 'F', '1 Brookside St', '100-222-3333'),
+    ('TestBorderline', 'Test', '1945-06-24', 'M', '2 High St', '200-333-4444'),
+    ('TestInDanger', 'Test', '2004-06-18', 'M', '3 Club Road', '300-444-5555'),
+    ('TestEarlyOnset', 'Test', '2002-06-28', 'F', '4 Valley Dr', '400-555-6666');
